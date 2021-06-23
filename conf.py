bd9fb962ccd0b5a49427fbd02a8ea1ea4ddc987b
Ivan Alvarez <ivalvarez22@gmail.com>
2021-06-22 21:30:21 -0400

    Fix utterances config

--- conf.py
+++ conf.py
@@ -1406,8 +1406,10 @@ WARN_ABOUT_TAG_METADATA = False
 
 # Put in global_context things you want available on all your templates.
 # It can be anything, data, functions, modules, etc.
-GLOBAL_CONTEXT['utterances_config'] = {"issue-term": "title",
-                                       "label": "Comments", "theme": "github-light", "crossorigin": "anonymous"}
+
+GLOBAL_CONTEXT = {
+    'utterances_config': {"issue-term": "title", "label": "Comments", "theme": "github-light", "crossorigin": "anonymous"}
+}
 
 # Add functions here and they will be called with template
 # GLOBAL_CONTEXT as parameter when the template is about to be
